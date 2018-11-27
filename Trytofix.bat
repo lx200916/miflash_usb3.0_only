c39669f26424b54bdef4b82ec00530a63950fe36
李翔 <lixiang@lixiangdeAir.lan>
2018-11-27 21:28:14 +0800

    Create Trytofix.bat

---
+++ Trytofix.bat
@@ -0,0 +1,8 @@
+@echo off
+Echo 此文件在Windows10下测试通过，通过修改注册表来完成操作，可能存在未知副作用，如果担心可在使用完后手动清除键值.
+pause
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100" /v "osvc" /t REG_BINARY /d "0000" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100" /v "SkipContainerIdQuery" /t REG_BINARY /d "01000000" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100" /v "SkipBOSDescriptorQuery" /t REG_BINARY /d "01000000" /f
+Echo 如果提示拒绝访问之类，尝试使用管理员权限运行
+pause
